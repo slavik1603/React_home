eb808f74e30bf85e3839afd55391205a2f99453b
Yaroslav <yaroslavrubel@gmail.com>
2021-08-08 21:53:00 +0300

    add About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,35 @@
+import { makeStyles } from '@material-ui/core'
 import React from 'react'
+import { Container, Row, Card } from 'react-bootstrap'
+import OrestLOGO from '../assets/OrestLOGO.jpg'
 
-export const About = () => <h1>H</h1>
+const useStyles = makeStyles(() => ({
+    cardImg: {
+        width: '100%',
+    },
+    cardInfo: {},
+    cardContacts: {},
+}))
+
+export const About = () => {
+    const classes = useStyles()
+    return (
+        <>
+            <Container>
+                <Row>
+                    <Card.Img className={classes.cardImg}>
+                        <img src={OrestLOGO} />
+                    </Card.Img>
+                    <Card>
+                        <Card.Info className={classes.cardInfo}>
+                            о.Орест
+                        </Card.Info>
+                        <Card.Contacts className={classes.cardContacts}>
+                            Контакти
+                        </Card.Contacts>
+                    </Card>
+                </Row>
+            </Container>
+        </>
+    )
+}
