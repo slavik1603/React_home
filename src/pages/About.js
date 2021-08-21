d8c778aca93f3dce77f2f6fc9db7c00a42d8653d
Yaroslav <yaroslavrubel@gmail.com>
2021-08-21 11:58:37 +0300

    add fon block

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,18 +2,27 @@ import React from 'react'
 import { Container, Row, Card, Col } from 'react-bootstrap'
 import { makeStyles } from '@material-ui/core'
 import OrestBiograph from '../assets/OrestBiograph.jpg'
+import Suviy from '../assets/suviy-fon.jpg'
 
 const useStyles = makeStyles(() => ({
+    cardAbout: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    },
     cardImg: {
         width: 'auto',
     },
     cardFoto: {
-        width: '100%',
-    },
-    cardAbout: {
-        backgroundColor: 'aqua',
+        margin: '5px',
+        width: '45%',
     },
     cardInfo: {
+        margin: '5px',
+        width: '45%',
+        backgroundImage: 'url("${ suviy })',
+    },
+    cardText: {
         color: 'red',
     },
     cardContacts: {
@@ -28,28 +37,29 @@ export const About = () => {
             <Container>
                 <Row>
                     <Col>
-                        <Card className={classes.cardFoto}>
-                            <Card.Img
-                                variant="top"
-                                src={OrestBiograph}
-                                className={classes.cardImg}
-                            />
-                        </Card>
-                    </Col>
-                    <Col>
                         <Card className={classes.cardAbout}>
-                            <Card.Info className={classes.cardInfo}>
-                                о. Орест Рубель, 1959 р.н., священник УГКЦ,
-                                служить на парафії с. Ульвівок, Теляж, Конотопи
-                                Сокальського деканату, Сокальсько-Жовківської
-                                Єпархії. Священичі свячення прийняв з рук бл.п.
-                                владики Юліана Вороновського 29 грудня 1991 р.
-                                Одружений, має четверо дітей. Проживає на
-                                парафії села Конотопи. Автор 16 книг.
-                            </Card.Info>
-                            <Card.Contacts className={classes.cardContacts}>
-                                Контакти
-                            </Card.Contacts>
+                            <Card className={classes.cardFoto}>
+                                <Card.Img
+                                    variant="top"
+                                    src={OrestBiograph}
+                                    className={classes.cardImg}
+                                />
+                            </Card>
+                            <Card className={classes.cardInfo}>
+                                <Card.Text className={classes.cardText}>
+                                    о. Орест Рубель, 1959 р.н., священник УГКЦ,
+                                    служить на парафії с. Ульвівок, Теляж,
+                                    Конотопи Сокальського деканату,
+                                    Сокальсько-Жовківської Єпархії. Священичі
+                                    свячення прийняв з рук бл.п. владики Юліана
+                                    Вороновського 29 грудня 1991 р. Одружений,
+                                    має четверо дітей. Проживає на парафії села
+                                    Конотопи. Автор 16 книг.
+                                </Card.Text>
+                                <Card.Subtitle className={classes.cardContacts}>
+                                    Контакти
+                                </Card.Subtitle>
+                            </Card>
                         </Card>
                     </Col>
                 </Row>
